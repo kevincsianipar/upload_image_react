32b628c1c34be5f2d07589e0403368e7da3f3374
Kevin Christian Sianipar <kevincsianipar@gmail.com>
2018-07-30 11:02:39 +0700

    Start working on modal upload

---
+++ upload_image_react/src/App.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { Modal, Button } from 'antd';
+import 'antd/dist/antd.css';
+import UploadModal from './uploadModal';
+
+class App extends React.Component {
+  state = {
+    ModalText: 'Content of the modal',
+    visible: false,
+    confirmLoading: false,
+  }
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  }
+
+  handleOk = () => {
+    this.setState({
+      ModalText: 'The modal will be closed after two seconds',
+      confirmLoading: true,
+    });
+    setTimeout(() => {
+      this.setState({
+        visible: false,
+        confirmLoading: false,
+      });
+    }, 2000);
+  }
+
+  handleCancel = () => {
+    console.log('Clicked cancel button');
+    this.setState({
+      visible: false,
+    });
+  }
+
+  render() {
+    const { visible, confirmLoading, ModalText } = this.state;
+    return (
+      <div>
+        <Button type="primary" onClick={this.showModal}>Upload</Button>
+        <Modal title="W:H 9:5 Photo"
+          visible={visible}
+          onOk={this.handleOk}
+          confirmLoading={confirmLoading}
+          onCancel={this.handleCancel}
+          okText='Submit'
+          width={665}
+        >
+          <div style={{height: 308}}>
+            <UploadModal />
+          </div>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default App;
